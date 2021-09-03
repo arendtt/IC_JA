21cad4c653e303e6cded5b758eb904ba42e5fe55
arendtt <88907138+arendtt@users.noreply.github.com>
2021-09-03 09:40:05 -0300

    Add files via upload

---
+++ IC_JA (1).sh
@@ -0,0 +1,121 @@
+
+
+# Os arquivos que você vai usar estão nessa pasta abaixo no Cerebro. São doze arquivos INPD_GenoBroad19_* . Cada um pesa 6.1Gb zipado. Já transferi o primeiro para a sua pasta
+
+/genetica_1/Genotipagem_INPD_Broad2019/PGC_NIMH_Salum_PTSD_GSA-MD_v1_3/RP-1194/processamento_inpd2019/proc_inicial_5548inds_dez19/ OK
+
+# IDs estao trocados como enviamos para o Broad. Precisa atuazliar IDs
+
+# instalar o gatk OK
+
+## Criar uma pasta do projeto no seu home
+
+## entrar na pasta deste novo projeto
+
+# Fazer download e dezipar arquivos do gatk
+wget https://github.com/broadinstitute/gatk/releases/download/4.2.0.0/gatk-4.2.0.0.zip  OK
+
+unzip gatk-4.2.0.0.zip  OK
+cd gatk-4.2.0.0/  OK
+./gatk # Se funcionar o programa vai rodar com as opcoes  OK
+
+# indexar o vcf. Imagina que esse arquivo é gigantesco, "indexar" em genética significa que o programinha vai criar um index para facilitar encontrar as infos mais rapidamente no arquivo. Tipo um livro
+
+./gatk IndexFeatureFile -I /home/julia.arendt/IC_JA/INPD_GenoBroad19_12.vcf.gz # cuidado com os caminhos  OK
+
+# Separa as informações que são importantes para extrair o dado de CNV. Você pode ( e deve) estudar o que signifca cada uma dessas colunas que eu selecionei. Principalmente o BAF e LRR.
+
+./gatk VariantsToTable -V /home/julia.arendt/IC_JA/INPD_GenoBroad19_12.vcf.gz -F ID -F CHROM -F POS -GF BAF -GF LRR -O INPD_CNVs_12.vcf  #OK
+
+
+# trocando cabeçalhos (isso pode ser feito de uma forma mais bonita)
+
+sed 's/BAF/B Allele Freq/g' INPD_CNVs_12.vcf > INPD_CNVs_12.baf  OK
+
+sed 's/LRR/Log R Ratio/g' INPD_CNVs_12.baf > INPD_CNVs_12.baf_lrr  OK
+
+
+# Semelhante ao script da Malu daqui pra baixo. Ver e inserir as hashtags dela aqui
+
+perl ~/PennCNV-1.0.5/compile_pfb.pl
+
+perl ~/PennCNV-1.0.5/kcolumn.pl INPD_CNVs_12.baf_lrr split 2 -tab -head 3 -name -out INPD_ref # parar aqui para apróxima semana
+
+
+ls INPD_ref* > IDs_INPD12
+
+sed 's/INPD_ref.//g' IDs_INPD12 > lista_files_INPD12
+
+perl /home/julia.arendt/IC_JA/PennCNV-1.0.5/compile_pfb.pl -list IDs_INPD12 -output INPD12.pfb
+
+
+# daqui pra baixo você vai preciar mudar os radicais dos arquivos e com os dados da illumina
+
+perl.15(tab) detect_cnv.pl -test -hmm affygw6.hmm -pfb affyref.pfb -list eshg_signallistfile.txt -log sd22_affy.log -out sd22_affy.rawcnv ##rodar pos paiva
+
+# ou
+
+perl /home/julia.arendt/IC_JA/PennCNV-1.0.5/detect_cnv.pl -test -hmm affygw6.hmm -pfb INPD12.pfb -list IDs_INPD12 -log CNVs.log -out CNVs.rawcnv
+
+./filter_cnv.pl sd22_affy.rawcnv -qclogfile sd22_affy.log -qclrrsd 0.3 -qcpassout sd22_affy.qcpass -qcsumout sd22_affy.qcsum -qcnumcnv 100 -out sd22_affy.goodcnv
+
+PennCNV - controle de qualidade
+
+1. QC por amostra
+
+./filter_cnv.pl sd22_affy.rawcnv -qclogfile sd22_affy.log -qclrrsd 0.3 -qcpassout
+sd22_affy.qcpass -qcsumout sd22_affy.qcsum -qcnumcnv 100 -out sd22_affy.goodcnv
+
+No R, usando o arquivo sd22_affy.qcsum. Visualização da distribuição de CNVs por pacientes
+eshg <- sd22_affy barplot(eshg$LRR_SD, ylim = c(0, 0.3), xlab = "Pacientes", ylab =
+"LRR_SD", main = "Sample QC") barplot(eshg$NumCNV, ylim = c(0,30), main = "CNVs por
+amostra", xlab = "Pacientes", ylab = "Número de CNVs" )
+
+2. Remover regiões ruins
+Centroméricas
+./scan_region.pl sd22_affy.goodcnv cen_region -minqueryfrac 0.5 > cnvcall.cen
+fgrep -v -f cnvcall.cen sd22_affy.goodcnv > sd22_affy_cen.clean
+restantes: 898 linhas
+
+Teloméricas:
+./scan_region.pl sd22_affy_cen.clean tel_region -minqueryfrac 0.5 > cnvcall.tel
+fgrep -v -f cnvcall.tel sd22_affy_cen.clean > sd22_affy_cen_tel.clean
+restantes: 889 linhas
+Duplicações segmentais, exceto 22q11.2
+./scan_region.pl sd22_affy_cen_tel.clean seg_dup_without22q112.txt -minqueryfrac
+0.5 > cnvcall.segdup fgrep -v -f cnvcall.segdup sd22_affy_cen_tel.clean >
+sd22_affy_cen_tel_segdup.clean
+restantes: 877 linhas
+Regiões de imunoglobulinas:
+./scan_region.pl sd22_affy_cen_tel_segdup.clean immuno_region -minqueryfrac
+0.5 > cnvcall.immuno fgrep -v -f cnvcall.immuno sd22_affy_cen_tel_segdup.clean
+> sd22_affy_cen_tel_segdup_immuno.clean
+restantes: 869 linhas
+
+3. Merge
+./clean_cnv.pl -signalfile ashg.329 combineseg
+sd22_affy_cen_tel_segdup_immuno.clean >
+sd22_affy_cen_tel_segdup_immuno_merged.clean
+signalfile: a file that contains Chr and Position for
+SNPs/markers
+Observação: fraction 0.2 (default)
+restantes: 847 linhas
+
+4. Mínimo de sondas:
+Deleções: 10 sondas
+./filter_cnv.pl -numsnp 10 -type del sd22_affy_cen_tel_segdup_immuno_merged.clean
+-output sd22_affy_del.clean
+restantes apenas del: 568
+Duplicações: 20 sondas
+
+./filter_cnv.pl -numsnp 20 -type dup sd22_affy_cen_tel_segdup_immuno_merged.clean
+-output sd22_affy_dup.clean
+restantes apenas dup: 58
+
+5. Juntar arquivos com deleções e duplicações:
+cat sd22_affy_del.clean sd22_affy_dup.clean > sd22_affy_del_dup.clean
+Contagem de CNVs: 626, sendo 58 duplicações e 568 deleções
+
+
+
+
